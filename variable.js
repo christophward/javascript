81b94660cb5b20c714112a53e84dce1c68767bc1
Apple Mac <applemac@Apples-MacBook-3.local>
2016-01-02 15:11:40 -0700

    learning variable

---
+++ variable.js
@@ -0,0 +1,9 @@
+// To create a variable, we use only one equals sign
+// But to check if two values are equal, we use 3 equal signs.
+
+// declare your variable here:
+var Christopher = "Leng"
+var myAge = 30
+var isOdd = true
+console.log(myAge);
+
